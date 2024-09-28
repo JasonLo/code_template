63f941b3425ebd709daf751a29448c37dc703716
Jason Lo <lcmjlo@gmail.com>
2024-09-28 13:38:52 -0500

    minor refactor

--- {{cookiecutter.project_slug}}/.devcontainer/post_create.sh
+++ {{cookiecutter.project_slug}}/.devcontainer/post_create.sh
@@ -7,8 +7,9 @@ config_git() {
     git config --global --add safe.directory /workspace/{{cookiecutter.package_name}}
     git config --global user.name "{{cookiecutter.author_name}}"
     git config --global user.email "{{cookiecutter.author_email}}"
-    git config --global core.eol lf
+    git config --global pull.rebase true
     git config --global core.filemode false
+    git config --global core.eol lf
     git config --global credential.helper store
     echo "Git configured."
 }
@@ -40,15 +41,38 @@ maybe_init_uv() {
     if [ ! -f pyproject.toml ]; then
         echo "Initializing a uv project..."
         uv init
-        uv add -r requirements.txt
         echo "uv project initialized."
     else
         echo "uv project already initialized."
     fi
 }
 
+install_fastapi() {
+    echo "Installing fastapi as uv tool..."
+    uv add fastapi[standard]
+    uv tool install fastapi[standard]
+}
+
+install_streamlit() {
+    echo "Installing streamlit as uv tool..."
+    uv add streamlit
+    uv tool install streamlit
+}   
+
+# Configure development tools
 config_git
 maybe_init_git
 maybe_init_uv
+
+# Install the uv tool for the app type, you may want to lock the version
+APP_TYPE="{{ cookiecutter.app_type }}"
+if [ "$APP_TYPE" == "fastapi" ]; then
+    install_fastapi
+elif [ "$APP_TYPE" == "streamlit" ]; then
+    install_streamlit
+else
+    echo "Unknown app type: $APP_TYPE"
+    exit 1
+fi
 
 echo "Post-create script complete."
