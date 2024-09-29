0bcc85a7ca5a796d527cf8adf0d54f2441a9e44f
Jason Lo <lcmjlo@gmail.com>
2024-09-28 23:18:50 -0500

    add bake tests

---
+++ tests/test_bake_fastapi.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import pytest
+from cookiecutter.main import cookiecutter
+
+
+@pytest.fixture
+def baked_project(tmp_path: Path) -> Path:
+    """Fixture to generate a project using Cookiecutter."""
+
+    app_type = "fastapi"
+    output_dir = tmp_path / app_type
+    cookiecutter(
+        ".",
+        no_input=True,
+        extra_context={"app_type": app_type},
+        output_dir=output_dir,
+    )
+    return output_dir
+
+
+def test_project_baked(baked_project):
+    """Check if the project was baked successfully."""
+
+    project_dir = baked_project / "new_project"
+    readme = project_dir / "README.md"
+    tasks_json = project_dir / ".vscode" / "tasks.json"
+    dockerfile = project_dir / "Dockerfile"
+
+    assert readme.exists()
+    assert tasks_json.exists()
+    assert dockerfile.exists()
